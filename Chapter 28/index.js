4628242146dfc0d4231ae47b04221751ec2a325a
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-08-07 01:04:11 +0500

    complete chapter 30

--- Chapter 28/index.js
+++ Chapter 28/index.js
@@ -1,11 +1,11 @@
 //Type Coercion
 // Automatic conversion of values from one data type to another typically performed during operations or comparisons involving different data types
 
-// const age = prompt("Enter your age");
-// console.log(typeof age); // if you check the type of age here the type of age is string
-// const yearEligibleVote = age - 18; // and here the string convert to number that's why call to type coercion
-// console.log(yearEligibleVote);
-
+const age = prompt("Enter your age");
+console.log(typeof age); // if you check the type of age here the type of age is string
+const yearEligibleVote = age - 18; // and here the string convert to number that's why call to type coercion
+console.log(yearEligibleVote);
+console.log(typeof yearEligibleVote);
 // 2 program
 // const result = "200" + 150;
 // console.log(result);
@@ -30,6 +30,6 @@
 // console.log(priceAfterTax);
 
 // 7 program
-const item = prompt("Enter item price");
-const priceAfterTax = parseFloat(item) + 1; // here we solved the above problem ider hum conversion karthey hain string to int or phir hum add kar dethey hain
-console.log(priceAfterTax);
+// const item = prompt("Enter item price");
+// const priceAfterTax = parseFloat(item) + 1; // here we solved the above problem ider hum conversion karthey hain string to int or phir hum add kar dethey hain
+// console.log(priceAfterTax);
---
+++ Chapter 30/index.js
@@ -0,0 +1,28 @@
+//Controlling The Length Of Decimal (it's means how many number you want after the point for that we use tofixed method)
+// Note: Here is one toFixed method
+
+// const testNumber = 45.123;
+// console.log(testNumber.toFixed(2));
+
+// Sales Tax Example
+
+// const itemPrice = 1750.765;
+// const salesTax = 0.02;
+// const salesTaxAmount = salesTax * itemPrice;
+// // console.log({ salesTaxAmount });
+
+// let totalPrice = salesTaxAmount + itemPrice;
+// // console.log(totalPrice);//but here we have a lot of decimal numbers
+// console.log(totalPrice.toFixed(0)); // we here soloved the problems here
+
+// School Marks Percentage Example
+
+// const totalMarks = 1100;
+// const obtainedMarks = 684;
+// // const percentage = (obtainedMarks / totalMarks) * 100;
+// // console.log(percentage);// here is a lot of number after number
+
+// const percentage = ((obtainedMarks / totalMarks) * 100).toFixed(2);
+// console.log(percentage); // here is the solved the problem
+
+// Jogar wala remaining I will do In Sha Allah
