9d2de529c877cfe0dac7412728c5145007422bc4
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-08-06 06:47:47 +0500

    chapter 29 complete

---
+++ Chapter 29/index.js
@@ -0,0 +1,22 @@
+//converting string to number topic name
+// number with string type
+// let numberString = "25";
+
+// // checking type using typeof operator
+// console.log(typeof numberString);
+
+// //converting string to number
+// let convertedNumber = Number(numberString);
+// //again we check the type and using typeof operator
+// console.log(typeof convertedNumber);
+
+// Converting Number to String
+
+// let number = 25;
+// //checking type here
+// console.log(typeof number);
+
+// //converting to string here
+// let convertedString = number.toString();
+// //checking type here using type of operator
+// console.log(typeof convertedString);
