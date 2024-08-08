7133f47e6d4616459dc3ea5c168b389728479b95
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-08-08 06:23:09 +0500

    chapter 31

---
+++ Chapter 31/index.js
@@ -0,0 +1,30 @@
+// Getting The Current Date And Time
+
+const rightNow = new Date();
+// console.log(rightNow);
+
+//Get Different Chunks from Date Object
+
+// Getting Time in miliscond
+
+// const currentTime = rightNow.getTime();
+// console.log(currentTime);
+
+// getting current full year
+// const fullYear = rightNow.getFullYear();
+// console.log(fullYear);
+
+// getting current date
+// const todayDate = rightNow.getDate();
+// console.log(todayDate);
+
+// getting day
+// const day = rightNow.getDay();
+// console.log(day);
+
+// Convert Date Object To String Using ToString
+
+// console.log(typeof rightNow); // if you check type here it will give object 
+
+const 
+
