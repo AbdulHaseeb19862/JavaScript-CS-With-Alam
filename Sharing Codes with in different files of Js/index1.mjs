2efaea580a25742c4fe80aa5ca0d9ce07a123274
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-08-11 17:36:53 +0500

    Sharing Codes in different Js files

---
+++ Sharing Codes with in different files of Js/index1.mjs
@@ -0,0 +1,10 @@
+import { printUserDetail, myName } from "./index2.mjs";
+
+function greeting() {
+  alert("Hello");
+}
+
+greeting();
+
+printUserDetail("Abdul Haseeb", 21, false);
+console.log(myName);
---
+++ Sharing Codes with in different files of Js/index2.mjs
@@ -0,0 +1,9 @@
+function printUserDetail(userName, age, isMarried) {
+  console.log(
+    `Your Name Is : ${userName} and your age is : ${age} and also your marital status is : ${isMarried} `
+  );
+}
+
+let myName = "Abdul Haseeb";
+
+export { printUserDetail, myName };
